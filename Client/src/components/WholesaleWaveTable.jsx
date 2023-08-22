48ebe4d4e8d0e1cb635e6d525549a39b48ed7e90
CoreyOliver <coreyoliver.dev@gmail.com>
2023-08-22 15:33:11 -0400

    add form data

--- Client/src/components/WholesaleWaveTable.jsx
+++ Client/src/components/WholesaleWaveTable.jsx
@@ -8,9 +8,24 @@ export async function loader() {
   const waves = await res.json();
   return waves;
 }
+const today = new Date()
+
 const WholesaleWaveTable = () => {
   const waves = useLoaderData();
   const [waveData, setWaveData] = useState(waves);
+  const [formData, setFormData] = useState({
+    waveDate: new Date(),
+    waveLocation: "",
+    waveUser: "",
+    waveNumber: "",
+    waveCustomer: "",
+    waveCount: "",
+    waveStartShip: "",
+    waveCancelDate: "",
+    waveTenderDate: "",
+    waveShipDate: "",
+    wavePrinted: "N",
+  });
 
   const wholesaleWavesToList = waveData.map((wave) => (
     <WholesaleWave
@@ -28,7 +43,17 @@ const WholesaleWaveTable = () => {
       user={wave.user}
     />
   ));
-  useEffect(() => console.log(waveData), waveData);
+
+  useEffect(() => console.log(formData), [formData]);
+
+  const handleChange = (e) => {
+    setFormData((prevState) => {
+      return {
+        ...prevState,
+        [e.target.name]: e.target.value,
+      };
+    });
+  };
 
   return (
     <div className="pt-32 flex justify-center items-center px-8 mx-auto">
@@ -91,127 +116,149 @@ const WholesaleWaveTable = () => {
         <tbody>
           {wholesaleWavesToList}
           <tr>
-              <th
-                scope="col"
-                className="sm:text-xs xl:text-sm mx-2 px-4 bg-orange-300"
-              >
-                <input 
-                  className="w-24 text-xs text-center"
-                  type="text"
-                  placeholder="Date"
-                  name="waveDate"
-                 />
-              </th>
-              <th
-                scope="col"
-                className="sm:text-xs xl:text-sm mx-2 px-4 bg-blue-300"
-              >
-                <input 
-                  className="w-24 text-xs text-center"
-                  type="text"
-                  placeholder="WH"
-                  name="waveLocation"
-                   />
-              </th>
-              <th
-                scope="col"
-                className="sm:text-xs xl:text-sm mx-2 px-4 bg-yellow-300 hidden md:table-cell"
-              >
-                <input 
-                  className="w-24 text-xs text-center"
-                  type="text"
-                  placeholder="Waver"
-                  name="waveUser"
-                   />
-              </th>
-              <th
-                scope="col"
-                className="sm:text-xs xl:text-sm mx-2 px-4 bg-pink-300"
-              >
-                <input 
-                  className="w-24 text-xs text-center"
-                  type="text"
-                  placeholder="Wave Number"
-                  name="waveNumber"
-                   />
-              </th>
-              <th
-                scope="col"
-                className="sm:text-xs xl:text-sm mx-2 px-4 bg-indigo-300"
-              >
-                <input 
-                  className="w-24 text-xs text-center"
-                  type="text"
-                  placeholder="Customer"
-                  name="waveCustomer"
-                   />
-              </th>
-              <th
-                scope="col"
-                className="sm:text-xs xl:text-sm mx-2 px-4 bg-green-300"
-              >
-                <input 
-                  className="w-24 text-xs text-center"
-                  type="text"
-                  placeholder="Unit Count"
-                  name="waveCount"
-                   />
-              </th>
-              <th
-                scope="col"
-                className="sm:text-xs xl:text-sm mx-2 px-4 bg-red-300"
-              >
-                <input 
-                  className="w-24 text-xs text-center"
-                  type="text"
-                  placeholder="Start Ship"
-                  name="waveStartShip"
-                   />
-              </th>
-              <th
-                scope="col"
-                className="sm:text-xs xl:text-sm mx-2 px-4 bg-red-300"
-              >
-                <input 
-                  className="w-24 text-xs text-center"
-                  type="text"
-                  placeholder="Cancel Date"
-                  name="waveCancelDate"
-                   />
-              </th>
-              <th
-                scope="col"
-                className="sm:text-xs xl:text-sm mx-2 px-4 bg-purple-300"
-              >
-                <input 
-                  className="w-24 text-xs text-center"
-                  type="text"
-                  placeholder="Tender Date"
-                  name="waveTenderDate"
-                   />
-              </th>
-              <th
-                scope="col"
-                className="sm:text-xs xl:text-sm mx-2 px-4 bg-purple-300"
-              >
-                <input 
-                  className="w-24 text-xs text-center"
-                  type="text"
-                  placeholder="Ship Date"
-                  name="waveShipDate"
-                   />
-              </th>
-              <th
-                scope="col"
-                className="sm:text-xs xl:text-sm mx-2 px-4 bg-orange-300"
-              >
-                <input 
-                  className="w-24 text-xs text-center"
-                  type="text"
-                  placeholder="Printed"
-                  name="wavePrinted"
-                   />
-              </th>
+            <th
+              scope="col"
+              className="sm:text-xs xl:text-sm mx-2 px-4 bg-orange-300"
+            >
+              <input
+                className="w-24 text-xs text-center"
+                type="text"
+                placeholder="Date"
+                name="waveDate"
+                value={formData.waveDate}
+                onChange={(e) => handleChange(e)}
+              />
+            </th>
+            <th
+              scope="col"
+              className="sm:text-xs xl:text-sm mx-2 px-4 bg-blue-300"
+            >
+              <input
+                className="w-24 text-xs text-center"
+                type="text"
+                placeholder="WH"
+                name="waveLocation"
+                value={formData.waveLocation}
+                onChange={(e) => handleChange(e)}
+              />
+            </th>
+            <th
+              scope="col"
+              className="sm:text-xs xl:text-sm mx-2 px-4 bg-yellow-300 hidden md:table-cell"
+            >
+              <input
+                className="w-24 text-xs text-center"
+                type="text"
+                placeholder="Waver"
+                name="waveUser"
+                value={formData.waveUser}
+                onChange={(e) => handleChange(e)}
+              />
+            </th>
+            <th
+              scope="col"
+              className="sm:text-xs xl:text-sm mx-2 px-4 bg-pink-300"
+            >
+              <input
+                className="w-24 text-xs text-center"
+                type="text"
+                placeholder="Wave Number"
+                name="waveNumber"
+                value={formData.waveNumber}
+                onChange={(e) => handleChange(e)}
+              />
+            </th>
+            <th
+              scope="col"
+              className="sm:text-xs xl:text-sm mx-2 px-4 bg-indigo-300"
+            >
+              <input
+                className="w-24 text-xs text-center"
+                type="text"
+                placeholder="Customer"
+                name="waveCustomer"
+                value={formData.waveCustomer}
+                onChange={(e) => handleChange(e)}
+              />
+            </th>
+            <th
+              scope="col"
+              className="sm:text-xs xl:text-sm mx-2 px-4 bg-green-300"
+            >
+              <input
+                className="w-24 text-xs text-center"
+                type="text"
+                placeholder="Unit Count"
+                name="waveCount"
+                value={formData.waveCount}
+                onChange={(e) => handleChange(e)}
+              />
+            </th>
+            <th
+              scope="col"
+              className="sm:text-xs xl:text-sm mx-2 px-4 bg-red-300"
+            >
+              <input
+                className="w-24 text-xs text-center"
+                type="text"
+                placeholder="Start Ship"
+                name="waveStartShip"
+                value={formData.waveStartShip}
+                onChange={(e) => handleChange(e)}
+              />
+            </th>
+            <th
+              scope="col"
+              className="sm:text-xs xl:text-sm mx-2 px-4 bg-red-300"
+            >
+              <input
+                className="w-24 text-xs text-center"
+                type="text"
+                placeholder="Cancel Date"
+                name="waveCancelDate"
+                value={formData.waveCancelDate}
+                onChange={(e) => handleChange(e)}
+              />
+            </th>
+            <th
+              scope="col"
+              className="sm:text-xs xl:text-sm mx-2 px-4 bg-purple-300"
+            >
+              <input
+                className="w-24 text-xs text-center"
+                type="text"
+                placeholder="Tender Date"
+                name="waveTenderDate"
+                value={formData.waveTenderDate}
+                onChange={(e) => handleChange(e)}
+              />
+            </th>
+            <th
+              scope="col"
+              className="sm:text-xs xl:text-sm mx-2 px-4 bg-purple-300"
+            >
+              <input
+                className="w-24 text-xs text-center"
+                type="text"
+                placeholder="Ship Date"
+                name="waveShipDate"
+                value={formData.waveShipDate}
+                onChange={(e) => handleChange(e)}
+              />
+            </th>
+            <th
+              scope="col"
+              className="sm:text-xs xl:text-sm mx-2 px-4 bg-orange-300"
+            >
+              <input
+                className="w-24 text-xs text-center"
+                type="text"
+                placeholder="Printed"
+                name="wavePrinted"
+                value={formData.wavePrinted}
+                onChange={(e) => handleChange(e)}
+              />
+            </th>
           </tr>
         </tbody>
       </table>
