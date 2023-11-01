372d87136d9fd2b40a86180f3ec0bb690572b5bc
CoreyOliver <coreyoliver.dev@gmail.com>
2023-11-01 15:14:12 -0400

    set up for date pick from calendar

--- Client/src/components/ShipDateInfo.jsx
+++ Client/src/components/ShipDateInfo.jsx
@@ -1,26 +1,36 @@
-import React from 'react'
-import { useParams, useLoaderData } from 'react-router-dom'
+import { useState } from "react";
+import { useLoaderData } from "react-router-dom";
+import WholesaleWave from "./WholesaleWave";
 
-
-export async function loader( {customer, shipDate} ) {
-  console.log(customer, shipDate)
-  const res = await fetch(`http://localhost:3000/dateCheck/${customer}/${shipDate}`);
+export async function loader({ customer, shipDate }) {
+  console.log(customer, shipDate);
+  const res = await fetch(
+    `http://localhost:3000/dateCheck/${customer}/${shipDate}`
+  );
   const wavesToShow = await res.json();
   return wavesToShow;
 }
 
 const ShipDateInfo = () => {
-  const {customer, shipDate } = useParams()
-  const wavesList = useLoaderData()
+  const wavesList = useLoaderData();
+  const [waveData, setWaveData] = useState(wavesList);
 
-  console.log(wavesList)
+  const customerDateList = wavesList.map(x => <li className="p-0 m-0" key={x.waveNumber}>{x.waveNumber}</li>)
+
+  console.log(customerDateList);
 
   return (
-    <div className="pt-32 flex justify-center items-center px-8 mx-auto">
-      <h1>{customer}</h1>
-      <h2>{shipDate}</h2>
+    <div className="pt-32 flex-col px-8 mx-auto">
+      <div className="flex justify-between mb-8">
+        <h2 className="px-8">{waveData[0].customer}</h2>
+        <h2 className="px-8">{waveData[0].shipDate}</h2>
+      </div>
+      <div className="grid-cols-2">
+        <ul>{customerDateList}</ul>
+      </div>
+      {/* render out the wave details */}
     </div>
-  )
-}
+  );
+};
 
-export default ShipDateInfo
+export default ShipDateInfo;
--- Server/controllers/main.controllers.js
+++ Server/controllers/main.controllers.js
@@ -74,7 +74,7 @@ module.exports = {
     try {
       //finish this query
       console.log(req.params)
-      const [wavesByEventData] = await connectDB.query(`select whLocation, waveNumber,customer,unitCount,date_format(startShip, '%m/%d') as startShip,      date_format(cancelDate, '%m/%d') as cancelDate,date_format(tenderDate, '%m/%d') as tenderDate,    date_format(shipDate, '%m/%d') as shipDate, printed, date_format(waveDate, '%m/%d') as waveDate, user FROM wave_release.wholesaledata WHERE(customer = ${req.params.customer} AND shipDate = ${req.params.shipDate})`)
+      const [wavesByEventData] = await connectDB.query(`select whLocation, waveNumber,customer,unitCount,date_format(startShip, '%m/%d') as startShip, date_format(cancelDate, '%m/%d') as cancelDate,date_format(tenderDate, '%m/%d') as tenderDate,    date_format(shipDate, '%m/%d') as shipDate FROM wave_release.wholesaledata WHERE(customer = '${req.params.customer}' AND shipDate = '${req.params.shipDate}');`)
       console.log('got event wave data')
       res.json(wavesByEventData)
 
